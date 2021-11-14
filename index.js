20ef0e1b5948886d9d150bfd7d3d212168e9494c
Stas Webdev <phrasalverbs911@gmail.com>
2021-11-14 12:29:58 +0000

    comments

--- index.js
+++ index.js
@@ -2,3 +2,29 @@ const caesar = require('./caesar')
 const atbash = require('./atbash')
 const rot8 = require('./rot8')
 
+// CLI tool should accept 3 options (short alias and full name):
+
+// -c, --config: config for ciphers Config is a string with pattern {XY(-)}n, where:
+// X is a cipher mark:
+// C is for Caesar cipher (with shift 1)
+// A is for Atbash cipher
+// R is for ROT-8 cipher
+// Y is flag of encoding or decoding (mandatory for Caesar cipher and ROT-8 cipher and should not be passed Atbash cipher)
+// 1 is for encoding
+// 0 is for decoding
+// -i, --input: a path to input file
+// -o, --output: a path to output file
+// ----
+// Details:
+// The task must be solved using only pure Node.js. Any libraries and packages (except nodemon, prettier and its plugins, eslint and its plugins) are prohibited.
+// Config option is required and should be validated. In case of invalid confing human-friendly error should be printed in stderr and the process should exit with non-zero status code.
+// If any option is duplicated (i.e. bash $ node my_ciphering_cli -c C1-C1-A-R0 -c C0) then human-friendly error should be printed in stderr and the process should exit with non-zero status code.
+// If the input file option is missed - use stdin as an input source.
+// If the output file option is missed - use stdout as an output destination.
+// If the input and/or output file is given but doesn't exist or you can't access it (e.g. because of permissions or it's a directory) - human-friendly error should be printed in stderr and the process should exit with non-zero status code.
+// If passed params are fine the output (file or stdout) should contain transformed content of input (file or stdin).
+// For encoding/decoding use only the English alphabet, all other characters should be kept untouched.
+// Using streams for reading, writing and transformation of text is mandatory.
+// Each cipher is implemented in the form of a transform stream.
+// Streams are piped inside each other according to config (you can use .pipe streams instances method or pipeline)
+// ---
